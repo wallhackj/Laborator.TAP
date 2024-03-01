f41794bf4b7310a8cc52faf516ee3203e7cc146c
GOLD <victorhaide@gmail.com>
2024-03-01 10:53:21 +0200

    Modificat pentru a utiliza Stream API.

--- Lab7.java
+++ Lab7.java
@@ -1,37 +1,33 @@
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Lab7 {
 
     public static void main(String[] args) {
-        List<Integer> save = new ArrayList<>();
-        Set<Integer> geometricProgression = new TreeSet<>(Arrays.asList(2, 6, 18, 54, 162));
-
-        int progression = -1;
-
-        for (Integer i : geometricProgression) {
-            if (progression == -1) {
-                progression = i;
-            } else {
-                try {
-                    int ratio = i / progression;
-                    if (ratio != 1) {
-                        progression = -1;
-                        save.add(ratio);
-                    } else {
-                        System.out.println("Nu este o progresie geometrica.");
-                        return;
-                    }
-                } catch (ArithmeticException e) {
-                    System.out.println("Exceptie aritmetica!");
-                }
-            }
-        }
+       Set<Integer> geometricProgression = new LinkedHashSet<>(Arrays.asList(2, 4, 8, 16, 32));
+        AtomicReference<Integer> previous = new AtomicReference<>(1);
+//
+//
+//        int[] arr = geometricProgression.stream().mapToInt(current -> {
+//            var result = current / previous.get();
+//            previous.set(current);
+//            return result;
+//        }).toArray();
+//
+//        int[] array = Arrays.stream(arr).skip(1).toArray();
+//
+//        boolean result = Arrays.stream(array).allMatch(n -> n == array[0]);
 
 
-        if (save.stream().distinct().count() == 1) {
-            System.out.println("Aceasta este o progresie geometrica.");
-        } else {
-            System.out.println("Aceasta nu este o progresie geometrica.");
-        }
+
+        boolean result = geometricProgression.stream()
+                .map(current -> {
+                    int ratio = current / previous.get();
+                    previous.set(current);
+                    return ratio;
+                })
+                .skip(1)
+                .allMatch(n -> n.equals(geometricProgression.iterator().next()));
+       System.out.println("Result: " + result);
     }
 }
