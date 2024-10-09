6675e2ede907141214a6ef4c9f5f4ddfd0a447d8
Omnissiah <victorhaideu@gmail.com>
2024-10-09 19:47:22 +0300

    lab 2

--- PCD/Lab2.java
+++ PCD/Lab2.java
@@ -3,10 +3,21 @@ package PCD;
 public class Lab2 {
     public static void main(String[] args) {
         ThreadGroup mainGroup = new ThreadGroup("Main");
+        ThreadGroup groupGO = new ThreadGroup(mainGroup, "GO");
+        ThreadGroup groupGZ = new ThreadGroup(groupGO, "GZ");
+        ThreadGroup groupGF = new ThreadGroup(mainGroup, "GF");
 
-        ThreadGroup groupGO = new ThreadGroup("GO");
-        ThreadGroup groupGZ = new ThreadGroup("GZ");
-        ThreadGroup groupGF = new ThreadGroup("GF");
+        Thread tha = new Thread(groupGZ, createTask("Tha-GZ"), "Tha");
+        tha.setPriority(1);
+
+        Thread thb = new Thread(groupGZ, createTask("Thb-GZ"), "Thb");
+        thb.setPriority(3);
+
+        Thread thc = new Thread(groupGZ, createTask("Thc-GZ"), "Thc");
+        thc.setPriority(3);
+
+        Thread thd = new Thread(groupGZ, createTask("Thd-GZ"), "Thd");
+        thd.setPriority(7);
 
         Thread thA = new Thread(groupGO, createTask("ThA"), "ThA");
         thA.setPriority(3);
@@ -26,21 +37,56 @@ public class Lab2 {
         Thread th3GF = new Thread(groupGF, createTask("Th3-GF"), "Th3");
         th3GF.setPriority(9);
 
-        Thread tha = new Thread(groupGZ, createTask("Tha-GZ"), "Tha");
-        tha.setPriority(1);
+        thA.start();
+        th1Main.start();
+        th2Main.start();
 
-        Thread thb = new Thread(groupGZ, createTask("Thb-GZ"), "Thb");
-        thb.setPriority(3);
+        th1GF.start();
+        th2GF.start();
+        th3GF.start();
 
-        Thread thc = new Thread(groupGZ, createTask("Thc-GZ"), "Thc");
-        thc.setPriority(3);
+        tha.start();
+        thb.start();
+        thc.start();
+        thd.start();
 
-        Thread thd = new Thread(groupGZ, createTask("Thd-GZ"), "Thd");
-        thd.setPriority(7);
+        System.out.println("All threads info:");
+        printThreadsGroups(mainGroup);
+    }
+
+    private static void printThreadsGroups(ThreadGroup group) {
+        var threads = new Thread[group.activeCount()];
+        var groups = new ThreadGroup[group.activeGroupCount()];
+
+        group.enumerate(threads, false);
+        group.enumerate(groups, false);
+
+
+        for (var t : threads) {
+            if (t != null) {
+                System.out.printf("Thread name: %s, Group: %s, Priority: %d\n",
+                        t.getName(), t.getThreadGroup().getName(), t.getPriority());
+            }
+        }
+
+        for (var g : groups) {
+            if (g != null) {
+                printThreadsGroups(g);
+            }
+        }
+
     }
 
     private static Runnable createTask(String s) {
-        return null;
+        return () -> {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
     }
 
 }
