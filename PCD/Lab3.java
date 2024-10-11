f8dfb96e17b1c893c96821bb21c88022387b1524
PC <victorhaideu@gmail.co>
2024-10-11 16:25:46 +0300

    fix:lab3

--- PCD/Lab3.java
+++ PCD/Lab3.java
@@ -1,34 +1,64 @@
 package PCD;
 
 import java.util.ArrayList;
+import java.util.Random;
+
+import static PCD.Lab3.mas;
 
 public class Lab3 {
+    static int[] mas = new int[100];
+
     public static void main(String[] args) {
+        generate();
+
         var fir1 = new Thr1();
         var fir2 = new Thr2();
         var fir3 = new Thr3();
         var fir4 = new Thr4();
 
-        var t1 = new Thread(fir1);
-        var t2 = new Thread(fir2);
-        var t3 = new Thread(fir3);
-        var t4 = new Thread(fir4);
+        var v1 = new Thread(fir1);
+        var v2 = new Thread(fir2);
+        var v3 = new Thread(fir3);
+        var v4 = new Thread(fir4);
 
-        t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
+        v1.start();
+        v2.start();
+        v3.start();
+        v4.start();
 
         try {
-            t1.join();
-            t2.join();
-            t3.join();
-            t4.join();
+            v1.join();
+            v2.join();
+            v3.join();
+            v4.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        student();
+
+    }
+
+    private static void student() {
+        for (char c : "Haideu Victor".toCharArray()) {
+            System.out.print(c);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void generate() {
+        var random = new Random();
+        for (int i = 0; i < 100; i++) {
+            mas[i] = random.nextInt(1000);
+        }
     }
 }
+
 class Thr1 implements Runnable {
     @Override
     public void run() {
@@ -36,14 +66,49 @@ class Thr1 implements Runnable {
         var count = 0;
         var arr = new ArrayList<Integer>();
 
+        for (int i = 0; i < Lab1.mas.length; i++) {
+            if (Lab1.mas[i] % 2 == 0 && Lab1.mas[i] > 15 && Lab1.mas[i] < 115) {
+                sum += i;
+                count++;
 
+                if (count == 2) {
+                    arr.add(sum);
+                    sum = 0;
+                    count = 0;
+                    var sumpos = arr.stream().mapToInt(x -> x).sum();
+
+                    System.out.println("Condiție 1:" + "fir:" + i + ", Suma pozițiilor pare (de la început) este: " + sumpos);
+                }
+            }
+        }
     }
 }
 
 class Thr2 implements Runnable {
     @Override
     public void run() {
+        var sum = 0;
+        var count = 0;
+        var arr = new ArrayList<Integer>();
 
+        for (int i = Lab1.mas.length - 1; i > 0; i--) {
+            if (Lab1.mas[i] % 2 == 0 && Lab1.mas[i] > 6 && Lab1.mas[i] < 106) {
+                sum += i;
+                count++;
+
+                if (count == 2) {
+                    arr.add(sum);
+                    sum = 0;
+                    count = 0;
+                    var sumpos = arr.stream()
+                            .mapToInt(x -> x)
+                            .sum();
+
+                    System.out.println("Condiție 2:" + "fir:" + i + ", Suma pozițiilor pare (de la end) este: " + sumpos);
+
+                }
+            }
+        }
     }
 }
 
@@ -51,7 +116,8 @@ class Thr3 implements Runnable {
     @Override
     public void run() {
         for (int i = 234; i < 987; i++) {
-            System.out.println("Condiția 3: Parcurs de la începutul intervalului [234, 987], i = " + i);
+            var num = mas[i];
+            System.out.println(num);
         }
     }
 }
@@ -60,7 +126,8 @@ class Thr4 implements Runnable {
     @Override
     public void run() {
         for (int i = 890; i >= 123; i--) {
-            System.out.println("Condiția 4: Parcurs de la sfârșitul intervalului [123, 890], i = " + i);
+            var num = mas[i];
+            System.out.println(num);
         }
     }
 }
